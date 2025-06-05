cc640ca6b967f51fb35ef9f2fef54baac45c6117
Yoely@peneh <yoely@peneh.com>
2025-06-05 16:52:52 -0400

    fixed frontend - removed laravel logo

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -20,14 +20,7 @@ const showingNavigationDropdown = ref(false);
                 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div class="flex h-16 justify-between">
                         <div class="flex">
-                            <!-- Logo -->
-                            <div class="flex shrink-0 items-center">
-                                <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
-                                    />
-                                </Link>
-                            </div>
+                            
 
                             <!-- Navigation Links -->
                             <div
